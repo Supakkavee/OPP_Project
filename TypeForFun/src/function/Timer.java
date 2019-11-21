2a8be4a25dab18ac70b102983b7baf32858297b9
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2019-11-21 22:58:16 +0700

    Add files via upload

--- TypeForFun/src/function/Timer.java
+++ TypeForFun/src/function/Timer.java
@@ -1,4 +1,7 @@
+package function;
 
+
+import function.Word;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -6,36 +9,40 @@ import javax.swing.*;
 public class Timer extends JPanel implements Runnable {
 
     Word w = new Word();
-    private int sec = 0;
+    public int sec = 0;
     JLabel time, l;
 
     public void run() {
         try {
-            setLayout(null);
+            setLayout(new GridLayout(1,2));
             time = new JLabel();
             l = new JLabel();
 
-            time.setBounds(150, 100, 100, 100);
-            l.setBounds(150, 200, 100, 100);
-
             add(time);
-            // add(l);
+            add(l);
             while (true) {
-                if (sec == 0) {
+                if (sec <= 0) {
                     // l.setText(w.getRandom(w.getWord()));
-                    l.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 30));
+                    // l.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 30));
                     sec = 15;
+                    // new GameM().setHp(30);    << ทำแบบนี้แล้วค้าง
+                    // new GameM().changeWord(); << ทำแบบนี้แล้วค้าง
                 }
                 time.setFont(new Font("Serif", Font.PLAIN, 30));
                 time.setText(String.format("%02d", sec));
                 Thread.sleep(1000);
                 sec--;
-
             }
         } catch (Exception ex) {
         }
+
+    }
+    public void setTime(int t) {
+        this.sec = t;
     }
 
-
+    public int getTime() {
+        return sec;
+    }
 
 }
