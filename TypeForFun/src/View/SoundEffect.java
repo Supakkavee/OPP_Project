65c55323d83282d8d74876770b96f7b76f5a456a
Supakkavee <42970626+Supakkavee@users.noreply.github.com>
2019-11-22 17:51:40 +0700

    Add files via upload

---
+++ TypeForFun/src/View/SoundEffect.java
@@ -0,0 +1,38 @@
+package View;
+
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SoundEffect {
+
+    Clip clip;
+
+    public void setFile(String soundFileName) {
+
+        try {
+            File file = new File(soundFileName);
+            AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(sound);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void play() {
+
+        clip.setFramePosition(0);
+        clip.start();
+    }
+
+}
