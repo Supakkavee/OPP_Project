ded7c39ee6d655028e22338f07de2b7d932a5f91
Supakkavee <42970626+Supakkavee@users.noreply.github.com>
2019-11-22 17:53:03 +0700

    Update MainGUI.java

--- TypeForFun/src/View/MainGUI.java
+++ TypeForFun/src/View/MainGUI.java
@@ -46,6 +46,9 @@ public class MainGUI extends GUIControl implements MouseListener {
         SoundEffect se = new SoundEffect();
         se.setFile(".//Music//Button.wav");
         se.play();
+        try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {}
         if (me.getSource().equals(start)) {
             bg.setVisible(false);
             getC().removeAll();
@@ -54,9 +57,6 @@ public class MainGUI extends GUIControl implements MouseListener {
             addC(name);
             getC().setBackground(Color.black);
         } else {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {}
             System.exit(0);
         }
     }
