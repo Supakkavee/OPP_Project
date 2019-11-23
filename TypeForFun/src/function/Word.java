a62603f7d28a36cb1345777e0efdeb2c2370be66
Supachai Chanto <42431305+Supachaijunto@users.noreply.github.com>
2019-11-23 20:00:33 +0700

    Update Word.java

--- TypeForFun/src/function/Word.java
+++ TypeForFun/src/function/Word.java
@@ -11,7 +11,12 @@ public class Word {
         "phlegmatic", "limp", "abraded", "pine", "putrefaction", "tonic", "pied", "verve", "shard", "alcove", "morbid",
         "bilge", "disconcert", "effrontery", "inane", "glimmer", "deviance", "obloquy", "repulsive", "interdict", "unfeigned",
         "maverick", "ambivalent", "ineptitude", "warmonger", "edacious", "benefactor", "enthral", "slur", "stentorian",
-        "visceral", "dilate", "acarpous", "graze", "hubris", "impede", "plead", "idyll", "lurk", "fetid", "enunciate"};
+        "visceral", "dilate", "acarpous", "graze", "hubris", "impede", "plead", "idyll", "lurk", "fetid", "enunciate", "inimical", 
+        "insularity", "ascribe", "aloof", "swerve", "regicide", "regale", "incense", "vain", "divulge", "grievous", "jocular",
+        "tassel", "elaborate", "contrite", "tenacity", "coddle", "illicit", "harrow", "reprobate", "flop", "inasmuch", "lithe",
+        "exoneration", "sinuous", "perilous", "middling", "entice", "profuse", "rapacious", "pundit", "apartheid", "mince",
+        "defiance", "ingest", "occluded", "deluge", "accretion", "vestige", "tenuous", "cognizant", "weigh", "accolade",
+        "chicanery", "malevolence", "forge", "transient", "foment", "idiosyncrasy", "stray"};
 
     public String getRandom(String[] arr) {
         Random r = new Random();
