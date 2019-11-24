08471ae645d91d2d988c671c505bfc4452411a9b
Supakkavee <42970626+Supakkavee@users.noreply.github.com>
2019-11-24 20:53:38 +0700

    Update Shoot.java

--- TypeForFun/src/function/Shoot.java
+++ TypeForFun/src/function/Shoot.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class Shoot extends JLabel implements ActionListener {
 
-    javax.swing.Timer t = new javax.swing.Timer(1, this);
+    Timer t = new Timer(1, this);
     private int x = 0, velX = 2;
     Image bullet = new ImageIcon("img/bullet.png").getImage().getScaledInstance(100, 50, Image.SCALE_DEFAULT);
 
@@ -24,19 +24,6 @@ public class Shoot extends JLabel implements ActionListener {
         x += velX;
         repaint();
     }
-
-
-    /*public static void main(String[] args) {
-        Shoot s = new Shoot();
-        JFrame fr = new JFrame();
-        s.setBounds(500, 330, 500, 50);
-        fr.add(s);
-        fr.setSize(1280, 720);
-        fr.setLayout(null);
-        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fr.setVisible(true);
-        fr.setResizable(false);
-    }**/
 
     public void setX() {
         this.x = 0;
