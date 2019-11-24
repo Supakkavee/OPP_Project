cc18ab62c89ec21a462d52e8e7536ac352d3663b
Supachai Chanto <42431305+Supachaijunto@users.noreply.github.com>
2019-11-24 16:44:25 +0700

    Update Word.java

--- TypeForFun/src/function/Word.java
+++ TypeForFun/src/function/Word.java
@@ -16,7 +16,27 @@ public class Word {
         "tassel", "elaborate", "contrite", "tenacity", "coddle", "illicit", "harrow", "reprobate", "flop", "inasmuch", "lithe",
         "exoneration", "sinuous", "perilous", "middling", "entice", "profuse", "rapacious", "pundit", "apartheid", "mince",
         "defiance", "ingest", "occluded", "deluge", "accretion", "vestige", "tenuous", "cognizant", "weigh", "accolade",
-        "chicanery", "malevolence", "forge", "transient", "foment", "idiosyncrasy", "stray"};
+        "chicanery", "malevolence", "forge", "transient", "foment", "idiosyncrasy", "stray", "discomfit", "spurn", "prosaic", 
+        "regale", "diaphanous", "maul", "sober", "calipers", "felicitous", "doggerel", "dexterity", "punctilious", "visceral", 
+        "effete", "probity", "harrow", "garrulous", "forswear", "conciliatory", "lope", "amalgamate", "guile", "elegy", "quixotic", 
+        "shunt", "impede", "torpid", "hermetic", "abhor", "apprehensive", "stint", "turpitude", "mince", "obtain", "equanimity", 
+        "bequest", "finical", "florid", "inscrutable", "cower", "defiance", "striated", "glean", "delineate", "mundane", 
+        "prevalent", "graze", "impecunious", "inveterate", "conviction", "husk", "abstruse", "submerge", "spear", "deviance", 
+        "ferocity", "recidivism", "bask", "pluck", "superimpose", "testiness", "insularity", "ail", "coagulation", "limn", 
+        "obstreperous", "derision", "aberration", "craven", "implicate", "conceit", "propitiatory", "droll", "recreancy", "vagary", 
+        "vex", "vituperate", "massacre", "recast", "fervid", "encapsulate", "alcove", "negligent", "taciturn", "luculent", 
+        "countenance", "bewilder", "nonplused", "taut", "whimsical", "hospitable", "gouge", "stentorian", "convoluted", 
+        "clientele", "avow", "incumbents", "inchoate", "commodious", "veneration", "opprobrious", "augury", "complaisance", 
+        "insinuate", "fawn", "mendacity", "forbearance", "reiterate", "ambidextrous", "cant", "heed", "excoriation", "animosity", 
+        "immutable", "obdurate", "abraded", "cravat", "cognizant", "synopsis", "veer", "contemn", "aver", "terse", "vanquish", 
+        "colander", "pungency", "equilibrium", "equivocal", "intersperse", "volubility", "trudge", "accretion", "fluke", "plead", 
+        "pariah", "paean", "pitfall", "extenuate", "immaculate", "pucker", "scribble", "sundry", "indulge", "palate", "espouse", 
+        "lament", "welter", "burgeon", "allegiance", "disinter", "hirsute", "illicit", "sullied", "encomium", "thwart", "obtrusive", 
+        "adorn", "ebullient", "calumny", "ascribe", "connoisseur", "enunciate", "valiant", "eloquence", "conjoin", "gist", 
+        "obsequious", "recitals", "deposition", "discreet", "glut", "finesse", "lavish", "impassive", "petrified", "sanguine", 
+        "petrify", "qualm", "abscond", "consummate", "boisterous", "sagacious", "putrefaction", "deluge", "incongruous", "mettlesome", 
+        "stigmatize", "scurvy", "foppish", "gossamer", "soporific", "tenacity", "solvent", "judicious", "dynamo", "apprise", 
+        "precarious", "trickle", "tarnished"};
 
     public String getRandom(String[] arr) {
         Random r = new Random();
